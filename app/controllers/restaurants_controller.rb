81c2572bdfef1db0d1d1f8db5987f051842263e8
Philip Krause <philip.krause88@yahoo.de>
2022-01-20 19:35:26 +0100

    asdas

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,7 +6,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @reviews = @restaurant.reviews
+    @review = Review.new
   end
 
   def new
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,18 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
+    # @reviews = @restaurant.reviews
     if @review.save
       redirect_to restaurant_path(@restaurant), notice: 'Review was successsfully created!🎉'
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
